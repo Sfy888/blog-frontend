371f482da7c05a58a835f16e70b8e15ecad14a75
Sfy888 <2269927540@qq.com>
2021-07-15 17:30:52 +0800

    init

---
+++ content/blog/article2.md
@@ -0,0 +1,6 @@
+# article 2
+
+ - 1dfsa
+ - 2fsdf
+ - 3gfd
+ - 4erwe
---
+++ gridsome.config.js
@@ -0,0 +1,48 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'BlogPost',
+        path: './content/blog/**/*.md',
+      }
+    },
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: process.env.GRIDSOME_API_URL,
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['post', 'tag'],   // StrapiPost
+        // typeName: 'Strapi',  // 默认是这个  
+        singleTypes: ['general'],   // general是 strapi的节点名
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
+    }
+  ],
+  templates: {
+    StrapiPost: [   // 集合名称
+      {
+        path: '/post/:id',
+        component: './src/templates/Post.vue'   // 模板组件一般放在template中
+      }
+    ],
+    StrapiTag: [
+      {
+        path: '/tag/:id',
+        component: './src/templates/Tag.vue'
+      }
+    ]
+  }
+}
